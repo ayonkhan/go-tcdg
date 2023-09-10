a21f7eb10adc138a35f10c0822086d997710ca67
Ayon Khan <ayonkhan@gmail.com>
2023-09-09 22:46:53 -0600

    Remove space

--- cards/deck.go
+++ cards/deck.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"math/rand"
-	"os"
-	"strings"
-	"time"
+    "fmt"
+    "io/ioutil"
+    "math/rand"
+    "os"
+    "strings"
+    "time"
 )
 
 // Create a new type of 'deck'
@@ -14,59 +14,59 @@ import (
 type deck []string
 
 func newDeck() deck {
-	cards := deck{}
+    cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+    cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+    cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for _, suit := range cardSuits {
-		for _, value := range cardValues {
-			cards = append(cards, value+" of "+suit)
-		}
-	}
+    for _, suit := range cardSuits {
+        for _, value := range cardValues {
+            cards = append(cards, value+" of "+suit)
+        }
+    }
 
-	return cards
+    return cards
 }
 
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
-	}
+    for i, card := range d {
+        fmt.Println(i, card)
+    }
 }
 
 func deal(d deck, handSize int) (deck, deck) {
-	return d[:handSize], d[handSize:]
+    return d[:handSize], d[handSize:]
 }
 
 func (d deck) toString() string {
-	return strings.Join([]string(d), ",")
+    return strings.Join([]string(d), ",")
 }
 
 func (d deck) saveToFile(filename string) error {
-	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+    return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
 func newDeckFromFile(filename string) deck {
-	bs, err := ioutil.ReadFile(filename)
+    bs, err := ioutil.ReadFile(filename)
 
-	if err != nil {
-		// Option #1 - log the error and return a call to newDeck()
-		// Option #2 - log the error and entirely quit the program
-		fmt.Println("Error: ", err)
-		os.Exit(1)
-	}
+    if err != nil {
+        // Option #1 - log the error and return a call to newDeck()
+        // Option #2 - log the error and entirely quit the program
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
 
-	s := strings.Split(string(bs), ",")
-	return deck(s)
+    s := strings.Split(string(bs), ",")
+    return deck(s)
 }
 
 func (d deck) shuffle() {
-	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
+    source := rand.NewSource(time.Now().UnixNano())
+    r := rand.New(source)
 
-	for i := range d {
-		newPosition := r.Intn(len(d) - 1)
+    for i := range d {
+        newPosition := r.Intn(len(d) - 1)
 
-		d[i], d[newPosition] = d[newPosition], d[i]
-	}
+        d[i], d[newPosition] = d[newPosition], d[i]
+    }
 }
